6ed3ef2cefaa9804557f32cb536e3fe4e627ed00
Nee_Panc <39270724+nitipanc@users.noreply.github.com>
2021-09-20 21:12:47 +0200

    update config

--- Debug/RPCore/RPA_ErrorHandle/RPA_ErrorDisplay.d
+++ Debug/RPCore/RPA_ErrorHandle/RPA_ErrorDisplay.d
@@ -1,8 +1,8 @@
 RPCore/RPA_ErrorHandle/RPA_ErrorDisplay.o: \
  ../RPCore/RPA_ErrorHandle/RPA_ErrorDisplay.cpp \
  ../RPCore/RPA_ErrorHandle/RPA_ErrorDisplay.h \
- D:\wiringPi\wiringPi/wiringPi.h
+ D:\GitRepository\wiringPi\wiringPi/wiringPi.h
 
 ../RPCore/RPA_ErrorHandle/RPA_ErrorDisplay.h:
 
-D:\wiringPi\wiringPi/wiringPi.h:
+D:\GitRepository\wiringPi\wiringPi/wiringPi.h:
